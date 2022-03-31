825f858353578e8f64631c0e9b831fd1d7fdd360
johnma <ma.jonathan02@gmail.com>
2022-03-30 23:12:22 -0400

    Continued construction of shell script for user customization of gif saving behavior

--- movevideos.sh
+++ movevideos.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-FLAG=1
-while [ $FLAG ]
+VALIDINPUT=0
+
+while [ $VALIDINPUT -eq 0 ]
 do
   echo "Would you like to create a new directory to save this video in? [Y/n]"
 
@@ -9,12 +10,18 @@ do
 
   if [ $NEWDIRECTORY == "Y" ] ; then
   NEWDIRECTORY=1
-  FLAG=0
+  VALIDINPUT=1
 
   elif [ $NEWDIRECTORY == "n" ] ; then
   NEWDIRECTORY=0
-  FLAG=0
+  VALIDINPUT=1
   fi
 done
 
-echo "$NEWDIRECTORY"
+if [ $NEWDIRECTORY ] ; then
+  echo "Enter the path for your new directory"
+  echo "Your gif will be saved in this directory"
+  read PATHNAME
+  mkdir $PATHNAME
+  mv $1 PATHNAME
+fi
